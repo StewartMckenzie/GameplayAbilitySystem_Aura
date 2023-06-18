828aec9977e833c8753b1523f44056e2ae3df585
Stewart Mckenzie <stewart.develops@gmail.com>
2023-06-18 09:27:40 -0500

    Ability System Component and Attribute Set

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Drift Studios
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Drift Studios
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
