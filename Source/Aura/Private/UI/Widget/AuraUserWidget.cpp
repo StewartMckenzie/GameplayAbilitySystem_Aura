647d367bb837ac4fb03918e25b297c5e0ddcb00f
Stewart Mckenzie <stewart.develops@gmail.com>
2023-06-29 10:24:53 -0500

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Drift Studios
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
